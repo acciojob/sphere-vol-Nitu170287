c1cc1d5aaa8363ff9f2eb15f1c26f420c221e09c
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-19 15:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function volume_sphere() {
 	let r = document.getElementById("radius").value;
 	r = Math.abs(r);
 	let v = (4/3) * Math.PI * Math.pow(r,3);
+	v = v.toFixed(4);
 	document.getElementById("volume").value = v;
 	return false ;
 } 
